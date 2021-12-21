765c943addd0d61e2a3756406e2efa4b0fafb7e2
wonderLuc <workmaildo@yandex.ru>
2021-12-21 13:27:59 +0500

    feat: add basic loader component

---
+++ src/Components/Loader/Loader.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.scss';
+
+export default function Loader(): JSX.Element {
+  return (
+    <article className="loader">
+      <p>Loading</p>
+    </article>
+  );
+}
