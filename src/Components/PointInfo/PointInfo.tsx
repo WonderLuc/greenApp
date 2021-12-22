4ea92db881ddb3a20a54a26be56e8a947ab41204
wonderLuc <workmaildo@yandex.ru>
2021-12-22 22:29:46 +0500

    refactor: split PointInfo to separate files

--- src/Components/PointInfo/PointInfo.tsx
+++ src/Components/PointInfo/PointInfo.tsx
@@ -1,65 +1,41 @@
-/* eslint-disable jsx-a11y/no-static-element-interactions */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { NavLink, useHistory, useParams } from 'react-router-dom';
+import {
+  NavLink, Redirect, useParams,
+} from 'react-router-dom';
 import { setDownloadActivity } from '../../Store/Actions';
 import { IState } from '../../Store/types';
+import PointMaterials from '../PointMaterials/PointMaterials';
+import PointTestimonials from '../PointTestimonials/PointTestimonials';
+import PointTitle from '../PointTitle/PointTitle';
+import PointWorktime from '../PointWorktime/PointWorktime';
 import './style.scss';
 
 export default function PointInfo(): JSX.Element {
-  const [worktimeOpen, setWorktime] = useState(false);
-  const history = useHistory();
   const dispatch = useDispatch();
   const { id } = useParams<{ id: string }>();
   const point = useSelector((state: IState) => state.pointsState.points.find((p) => p.id === +id));
 
   useEffect(() => {
-    if (!point) history.push('/');
     dispatch(setDownloadActivity(false));
   });
 
   return (
-    <main className="point">
-      <article className="point-name">
-        <h2 className="point-name__title">Переработки пункт малыш</h2>
-        <h3 className="point-name__adress">Московая 20</h3>
-      </article>
-      <NavLink to="/" className="nav-link">
-        <p className="nav-link_back">
-          <i className="arrow arrow_left" />
-          Карта
-        </p>
-      </NavLink>
-      <article className="point-worktime">
-        <h2 className="point-worktime__title">Время работы</h2>
-        <section className="point-worktime__list" onClick={() => setWorktime(!worktimeOpen)}>
-          <ul className={`worktime-list ${worktimeOpen ? 'worktime-list_open' : ''}`}>
-            <li className="worktime-list__item">Вт: 10:00 - 22:00</li>
-            <li className="worktime-list__item">Вт: 10:00 - 22:00</li>
-            <li className="worktime-list__item">Вт: 10:00 - 22:00</li>
-            <li className="worktime-list__item">Вт: 10:00 - 22:00</li>
-            <li className="worktime-list__item">Вт: 10:00 - 22:00</li>
-            <li className="worktime-list__item">Вт: 10:00 - 22:00</li>
-            <li className="worktime-list__item">Вт: 10:00 - 22:00</li>
-          </ul>
-          <i className={`arrow ${worktimeOpen ? 'arrow_up' : 'arrow_down'}`} />
-        </section>
-      </article>
-      <article className="materials">
-        <h2 className="materials__title">Принимаемые материалы:</h2>
-        <ul className="materials-list">
-          <li className="materials-list__item">Стеклотара</li>
-          <li className="materials-list__item">Стеклотара</li>
-          <li className="materials-list__item">Стеклотара</li>
-          <li className="materials-list__item">Стеклотара</li>
-          <li className="materials-list__item">Стеклотара</li>
-        </ul>
-      </article>
-      <article className="inwork">
-        <h2 className="inwork__title">Отзывы</h2>
-        <h2 className="inwork__message">В разработке</h2>
-      </article>
-    </main>
+    point
+      ? (
+        <main className="point">
+          <PointTitle point={point} />
+          <NavLink to="/" className="nav-link">
+            <p className="nav-link_back">
+              <i className="arrow arrow_left" />
+              Карта
+            </p>
+          </NavLink>
+          <PointWorktime point={point} />
+          <PointMaterials point={point} />
+          <PointTestimonials />
+        </main>
+      )
+      : <Redirect to="/" />
   );
 }
--- src/Store/mockDB.tsx
+++ src/Store/mockDB.tsx
@@ -10,6 +10,15 @@ const points: IPoint[] = [
     latitude: 56.881689,
     id: 1,
   },
+  {
+    name: 'Пункт приема вторсырья',
+    address: 'Маневровая 35',
+    worktime: ['Пн 08:00 - 20:00', 'Вт 10:00 - 21:00'],
+    materials: ['Бумага', 'Cтекло'],
+    longitude: 60.6135816,
+    latitude: 56.8412112,
+    id: 2,
+  },
 ];
 
 export default points;
