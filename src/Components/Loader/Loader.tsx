7b282e4b982a1c8ce784117649c660ed1377610e
MonkeysTower <Trifandre@yandex.ru>
2021-12-22 23:37:11 +0500

    Style: add style for loader

--- src/Components/Loader/Loader.tsx
+++ src/Components/Loader/Loader.tsx
@@ -7,8 +7,8 @@ export default function Loader(): JSX.Element {
   const isLoading = useSelector((state: IState) => state.download.isActive);
 
   return (
-    <article className={`loader ${isLoading ? 'loader_open' : ''}`}>
-      <p>Loading</p>
+    <article className="loader-zone">
+      <p className={`loader ${isLoading ? 'loader_open' : ''}`}>Loading</p>
     </article>
   );
 }
